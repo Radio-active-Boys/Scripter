9d3afdb5d3d4d0103546fe4b9a9220bf946206dc
ASHUTOSH VISHWAKARMA <ashutoshvishwakarma208@outlook.com>
2024-06-16 00:19:35 +0530

    updated request

--- src/request.cpp
+++ src/request.cpp
@@ -12,4 +12,15 @@ void Request::handle_request(std::string cmd, websocketpp::connection_hdl hdl, s
         char command[] = "./arduino-cli board list";
         Exec::exec_cmd(command,hdl,connection_list_mutex, server);
     }
+    else if(cmd[0]=='{')
+    {
+        rapidjson::Document doc;
+        doc.Parse(cmd.c_str());
+         if (doc.HasParseError()) { 
+        std::cerr << "Error parsing JSON: "
+             << doc.GetParseError() << std::endl; 
+        return; 
+        std::cout<<"Type of Request: "<<doc["Type"].GetString()<<std::endl;
+    } 
+    }
 }
